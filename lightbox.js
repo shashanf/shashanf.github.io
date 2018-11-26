8a3a27d61f102e1a51c230e2451678b3f3ce0279
bayuhibatullah <44220753+bayuhibatullah@users.noreply.github.com>
2018-11-26 13:30:13 +0700

    Create lightbox.js

---
+++ lightbox.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="style.css">
+	<title>Organisasi Ilmu Komputer</title>
+</head>
+<body>
+	<div class="kiri">
+		<div id="kontenkiri">
+			<img id="komputer" src="komputer.png">
+		</div>
+	</div>
+	<div class="kanan">
+		<div id="kontenkanan">
+			<h1>SELAMAT DATANG DI FASILKOM</h1>
+			<p id="tulisan">
+				Website ini berisi informasi tentang Unit Kegiatan Mahasiswa yang ada di Fakultas Ilmu Komputer Universitas Jember
+			</p>
+	<div class:"button">
+	<button ="green">Selengkapnya</button>
+		</div>
+	</div>
+</body>
+
+</html>
